836ed49b6fc97b339142fbfc7b59eb2b9d79cca1
yoelAAA-chab <yagnera@fcpn.edu.bo>
2025-11-11 20:46:41 -0400

    Delete practica_n2 directory

--- practica_n2/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '', 
-    database: 'practicaN2-web3'
-});
-
-export default pool;
